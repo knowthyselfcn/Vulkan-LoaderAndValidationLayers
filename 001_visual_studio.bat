ee1f230b968a8c6953002ffc005126137cd8afd9
Mike Weiblen <mikew@lunarg.com>
2017-10-11 13:30:05 -0600

    MikeW's private development tools.
    These tools are not intended to be pushed to master.
    
    Includes Makefile for Linux, .bat files for Windows.
    
    Change-Id: Icd799ac8ac465b6d985676c3bf538b0758393862

---
+++ 001_visual_studio.bat
@@ -0,0 +1,20 @@
+set OLDDIR=%CD%
+
+@cd "C:\Program Files (x86)\Microsoft Visual Studio 14.0"
+call "Common7\Tools\VsDevCmd.bat"
+
+@set PATH=C:\Program Files\CMake\bin;%PATH%
+
+@date /t
+@time /t
+
+cd %OLDDIR%
+set TGT_DIR=BUILD
+cmake -G "Visual Studio 14 Win64" -H. -B%TGT_DIR%
+
+@date /t
+@time /t
+
+cd %TGT_DIR%
+set VK_LAYER_PATH=%CD%\layers\Debug
+start VULKAN.sln
